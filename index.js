70be197ad5f0b06b97c6d7ab95b2a51bc78b9547
Joonatan Nikkilä <joonatannikkila@Joonatan-MacBook-Pro.local>
2025-12-21 16:33:31 +0200

    bug fixes

--- index.js
+++ index.js
@@ -81,6 +81,16 @@ app.post('/api/persons', (request, response) => {
 })
 
 const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason)
+})
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err)
+})
+

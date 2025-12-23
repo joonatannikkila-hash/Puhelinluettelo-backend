131b195942cebfb62acc6c62962143e1c57d7525
Joonatan Nikkilä <joonatannikkila@Joonatan-MacBook-Pro.local>
2025-12-23 15:37:29 +0200

    ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  {
+    ignores: [
+      'node_modules/**',
+      'dist/**'
+    ]
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.node
+    },
+    rules: {
+      'no-console': 0
+    }
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs'
+    }
+  }
+])

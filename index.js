44e675790047389d7963d623da0dbdbea686b516
Joonatan Nikkilä <joonatannikkila@Joonatan-MacBook-Pro.local>
2025-12-23 15:18:49 +0200

    fullstack versio frontti buildin kanssa

--- index.js
+++ index.js
@@ -1,96 +1,100 @@
+require('dotenv').config()
+
 const express = require('express')
-const morgan = require('morgan')
-const app = express()
 const cors = require('cors')
+const morgan = require('morgan')
+
+const Person = require('./models/person')
+
+const app = express()
 
 app.use(cors())
-
 app.use(express.json())
-
-morgan.token('body', req => {
-  return req.method === 'POST' ? JSON.stringify(req.body) : ''
-})
-
-app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :body')
-)
-
 app.use(express.static('dist'))
 
 
-let persons = [
-  { id: 1, name: 'Arto Hellas', number: '040-123456' },
-  { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
-  { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
-  { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
-]
+morgan.token('body', req =>
+  req.method === 'POST' ? JSON.stringify(req.body) : ''
+)
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
-app.get('/info', (req, res) => {
-    const now = new Date()
-    res.send(`
-        <div> Phonebook has info for ${persons.length} people</div>
-        <div> ${now}</div>
-    `)
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find (person => person.id === id)
 
-    if(person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
 
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => {
+      if (result) {
+        res.status(204).end()
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body
+const path = require('path')
 
-    const person = { 
-        id: Math.floor(Math.random()*1000000), 
-        name: body.name,
-        number: body.number
-    }
-
-    if(!person.name || !person.number){
-        return response.status(400).json({
-                error: 'content missing'
-            })
-    }
-
-    const nameExists = persons.some(p => p.name ===body.name)
-    if(nameExists){
-        return response.status(400).json({error: 'name must be unique'})
-    }
-
-    persons = persons.concat(person)
-    response.json(person)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).json({
+      error: 'malformatted id'
+    })
+  }
+
+  if(error.name == "ValidationError") {
+    return res.status(400).json({
+      error: error.message
+    })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+
 
 const PORT = process.env.PORT || 3001
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-process.on('unhandledRejection', (reason) => {
-  console.error('Unhandled Rejection:', reason)
-})
-
-process.on('uncaughtException', (err) => {
-  console.error('Uncaught Exception:', err)
-})
-
